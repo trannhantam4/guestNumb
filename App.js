3485c741acf8cc4396c33befbb36ddd56562bfe3
trannhantam4 <80136734+trannhantam4@users.noreply.github.com>
2023-07-24 17:27:28 +0700

    new

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
 import StartScreen from "./screens/StartScreen";
-
+import { LinearGradient } from "expo-linear-gradient";
 export default function App() {
-  return <StartScreen />;
+  return (
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.container}>
+      <ImageBackground
+        source={require("./assets/image/background.png")}
+        resizeMode="cover"
+        style={styles.container}
+        imageStyle={styles.image}
+      >
+        <StartScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+  },
+  image: {
+    opacity: 0.2,
   },
 });
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 import React from "react";
-
+import { useState } from "react";
 export default function GameScreen() {
   return (
     <View>
